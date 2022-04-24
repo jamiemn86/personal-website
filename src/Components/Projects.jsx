737a108903336ccc28e308cc1566dfed25e4ae12
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-04-24 19:49:07 +0200

    React Transition Group for image gallery css effect

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import projectimage1 from '../Assets/projectimage1.png';
 import projectimage2 from '../Assets/projectimage2.png';
 import projectimage3 from '../Assets/projectimage3.png';
+import { CSSTransition } from 'react-transition-group';
+import '../styles.css';
 
 function Projects() {
   const galleryImages = [projectimage1, projectimage2, projectimage3];
-
   const [n, setN] = useState(0);
   const [galleryImage, setGalleryImage] = useState(galleryImages[n]);
+  const [inProp, setInProp] = useState(false);
 
   const nextImage = () => {
+    setInProp(true);
     if (n === 2) {
       return;
     } else setGalleryImage(galleryImages[n + 1]);
@@ -20,11 +23,16 @@ function Projects() {
   };
 
   const prevImage = () => {
+    setInProp(true);
     if (n === 0) {
       return;
     } else setGalleryImage(galleryImages[n - 1]);
     setN(n - 1);
   };
+
+  useEffect(() => {
+    setTimeout(setInProp(false), 3000);
+  }, [inProp]);
 
   var imageNumber =
     galleryImage === projectimage1
@@ -41,51 +49,54 @@ function Projects() {
           Ironhack Web Development Bootcamp: Final Project
         </p>
       </div>
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide relative"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner relative w-full overflow-hidden">
-          <div className="carousel-item float-left w-full">
-            <img src={galleryImage} className="block w-full" alt="Camera" />
+      <CSSTransition in={inProp} timeout={500} classNames="my-node">
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide relative"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner relative w-full overflow-hidden">
+            <div className="carousel-item float-left w-full">
+              <img src={galleryImage} className="block w-full" alt="Camera" />
+            </div>{' '}
           </div>
+
+          <button
+            className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon inline-block bg-no-repeat"
+              aria-hidden="true"
+            ></span>
+            <span
+              className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
+              onClick={prevImage}
+            >
+              Previous
+            </span>
+          </button>
+          <button
+            className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon inline-block bg-no-repeat"
+              aria-hidden="true"
+            ></span>
+            <span
+              className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
+              onClick={nextImage}
+            >
+              Next ({imageNumber} of 3)
+            </span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span
-            className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
-            onClick={prevImage}
-          >
-            Previous
-          </span>
-        </button>
-        <button
-          className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span
-            className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
-            onClick={nextImage}
-          >
-            Next ({imageNumber} of 3)
-          </span>
-        </button>
-      </div>
+      </CSSTransition>
       <div className="max-w-lg sm:mx-auto text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto">
         <p className="leading-relaxed mt-10">
           For the final module of the Ironhack Web Development Bootcamp I did in
