8d16710c288373bd64b5b4a79e7e072b97fed82a
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-06-26 21:11:24 +0200

    Adding a 404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Cv from './Components/Cv';
 import Projects from './Components/Projects';
 import Homepage from './Components/Homepage';
 import { Helmet } from 'react-helmet';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
         <title>{'JMN Personal Website'}</title>
       </Helmet>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/cv" element={<Cv />} />
         <Route path="/projects" element={<Projects />} />
+        <Route component={NotFound} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Link } from 'react-router-dom';
+import NotFoundImage from '../Assets/404.jpg';
+
+function NotFound() {
+  return (
+    <>
+      <Header />
+      <div className="max-w-lg sm:mx-auto sm:text-center text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto">
+        <p className="leading-relaxed mt-4 text-center">Page not found</p>
+      </div>
+      <div className="flex justify-center">
+        <img
+          src={NotFoundImage}
+          className="p-1 bg-white border rounded max-w-sm"
+          alt="error404notfound"
+        />
+      </div>
+
+      <div className="max-w-lg sm:mx-auto sm:text-center text-gray-500 bg-white px-4 py-4 max-w-screen-xl mx-auto">
+        <div className="leading-relaxed mt-4 my-4 text-center">
+          <p>Sorry, this page doesn't appear to exist</p>
+          <p>
+            Take me back to the{' '}
+            <Link to="/" className="text-gray-600 hover:text-indigo-600">
+              homepage
+            </Link>
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default NotFound;
