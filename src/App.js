14efe66f999f454fd1abf01b1a158bfa30d2f3f2
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-08-08 12:08:24 +0200

    Loading spinner and React lazy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './Components/About';
-import Cv from './Components/Cv';
-import Projects from './Components/Projects';
 import Homepage from './Components/Homepage';
 import { Helmet } from 'react-helmet';
-import NotFound from './Components/NotFound';
+import LoadingSpinner from './Components/LoadingSpinner';
+
+const About = React.lazy(() => import('./Components/About'));
+const Cv = React.lazy(() => import('./Components/Cv'));
+const Projects = React.lazy(() => import('./Components/Projects'));
+const NotFound = React.lazy(() => import('./Components/NotFound'));
 
 function App() {
   return (
@@ -13,14 +16,16 @@ function App() {
         <meta charSet="utf-8" />
         <title>{'JMN Personal Website'}</title>
       </Helmet>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cv" element={<Cv />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route component={NotFound} />
-      </Routes>
+      <Suspense fallback={<div>{<LoadingSpinner />}</div>}>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cv" element={<Cv />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route component={NotFound} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
