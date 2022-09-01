5bd40bbdbc9d06c4e11d4698892307d5cfeea3e7
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-09-01 17:16:20 +0200

    Project gallery update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -55,60 +55,59 @@ function Projects() {
         </p>
       </div>
       <CSSTransition in={inProp} timeout={500} classNames="my-node">
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide relative"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner relative w-full overflow-hidden">
-            <div className="carousel-item float-left w-full">
+        <div className="carousel-inner relative w-full overflow-hidden">
+          <div className="carousel-item mx-auto max-w-screen-lg">
+            <div
+              id="carouselExampleIndicators"
+              className="carousel slide relative"
+              data-bs-ride="carousel"
+            >
               <img
                 src={galleryImage}
                 onLoad={onLoadHandler}
                 className="block w-full"
-                alt="Camera"
+                alt="gallery"
               />
+              <button
+                className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon inline-block bg-no-repeat"
+                  aria-hidden="true"
+                ></span>
+                {imagesLoaded ? (
+                  <span
+                    className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
+                    onClick={prevImage}
+                  >
+                    Previous
+                  </span>
+                ) : null}
+              </button>
+              <button
+                className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon inline-block bg-no-repeat"
+                  aria-hidden="true"
+                ></span>
+                {imagesLoaded ? (
+                  <span
+                    className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
+                    onClick={nextImage}
+                  >
+                    Next ({imageNumber} of 3)
+                  </span>
+                ) : null}
+              </button>
             </div>{' '}
           </div>
-
-          <button
-            className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon inline-block bg-no-repeat"
-              aria-hidden="true"
-            ></span>
-            {imagesLoaded ? (
-              <span
-                className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
-                onClick={prevImage}
-              >
-                Previous
-              </span>
-            ) : null}
-          </button>
-          <button
-            className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon inline-block bg-no-repeat"
-              aria-hidden="true"
-            ></span>
-            {imagesLoaded ? (
-              <span
-                className="visually-hidden text-gray-500 mx-10 backdrop-blur-md bg-black/10"
-                onClick={nextImage}
-              >
-                Next ({imageNumber} of 3)
-              </span>
-            ) : null}
-          </button>
         </div>
       </CSSTransition>
       <div className="max-w-lg sm:mx-auto text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto">
