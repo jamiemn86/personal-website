ab0b7b5eb052f5faeee03609f05280cac5e76811
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2022-10-30 15:18:47 +0100

    Switching to react helmet async to fix console warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './Components/Pages/Homepage';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import LoadingSpinner from './Components/LoadingSpinner';
 import ErrorBoundary from './Components/Error Handling/ErrorBoundary';
 
@@ -14,24 +14,26 @@ const NotFound = React.lazy(() =>
 
 function App() {
   return (
-    <BrowserRouter>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{'JMN Personal Website'}</title>
-      </Helmet>
-      <ErrorBoundary>
-        <Suspense fallback={<div>{<LoadingSpinner />}</div>}>
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/cv" element={<Cv />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route component={NotFound} />
-          </Routes>{' '}
-        </Suspense>{' '}
-      </ErrorBoundary>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{'JMN Personal Website'}</title>
+        </Helmet>
+        <ErrorBoundary>
+          <Suspense fallback={<div>{<LoadingSpinner />}</div>}>
+            <Routes>
+              <Route path="*" element={<NotFound />} />
+              <Route path="/" element={<Homepage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/cv" element={<Cv />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route component={NotFound} />
+            </Routes>{' '}
+          </Suspense>{' '}
+        </ErrorBoundary>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
